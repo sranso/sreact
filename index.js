053a67032399a9d0a61191e9effbda947c0dbb94
Sarah Ransohoff <sranso@gmail.com>
2016-09-29 16:34:14 -0400

    Create element and append to div app.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-window.onload = () => console.log('hello React world');
+const rootNode = document.createElement('div');
+rootNode.innerHTML = 'original DOM';
+rootNode.setAttribute('style', 'margin: 20px; font-size; 14px;');
+
+
+const app = document.getElementById('app');
+app.innerHTML = '';
+app.appendChild(rootNode);
