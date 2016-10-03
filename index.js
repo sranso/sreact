4ee88dec40ffbfc94da89129a0bd17dc0374218c
Sarah Ransohoff <sranso@gmail.com>
2016-10-03 16:13:18 -0400

    Represent node data structure and render as HTML.

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// patch
-// diff
-// create vd node itself
-
-// use https://github.com/paul-jean/dom-viz to see if correct
-// divs are changing
-
-// 1. describe virtual dom
-//    (as obj / list; start with list, expand as nec)
-// node : obj attributes : list html
+/*
+ * describe virtual dom
+ * node = [ string eltype : obj attributes : list children ]
+ */
 const rootNode = [
   'div', {
     'style': {
-      'textAlign': 'center',
+      'text-align': 'center',
       'color': 'blue',
       'margin': '20px'
     }
@@ -19,55 +13,60 @@ const rootNode = [
   ['hello world']
 ];
 
-// 2. and take it to real dom (convert to html)
+const nestedNode = [
+  'div', {
+    'style': {
+      'text-align': 'center',
+      'color': 'blue',
+      'margin': '20px'
+    }
+  },
+  [
+    [
+      'p', {
+        'style': {
+          'font-size': '20px;'
+        }
+      },
+      ['hello world']
+    ]
+  ]
+];
+
+/*
+ * render a node or simple textNode as html string
+ * node -> HTML string
+ */
 const render = (node) => {
-  let html;
+  if (typeof node === 'string') {
+    return node;
+  }
   if (node.length !== 3) {
+    console.log(node);
     throw new Error('Please pass a valid node.');
   }
 
-  return parseNode(node);
+  const [elType, atts, children] = node;
+  const attString = Object.keys(atts).map((att) => renderAtt(att, atts[att])).join(' ');
+  return `<${elType} ${attString}>${children.map(render).join('\n')}</${elType}>`;
 };
 
-const parseNode = (node, isTextNode = false) => {
-  const elType = node[0];
-  return node.reduce((acc, el, index) => {
-    if (isTextNode) {
-      return `${acc}${el}</${elType}>`;
-    } else if (index === 0) {
-      return `${acc}<${el}`;
-    } else if (typeof el === 'string') {
-      // ??
-      return `${acc}${el}</${elType}>`;
-    } else if (Array.isArray(el)) {
-      // handle children
-      if (Array.isArray(el[0])) {
-        return `${acc}${parseNode(el)}`;
-      } else {
-        return `${acc}${parseNode(el, true)}`;
-      }
-    } else if (!Array.isArray(el) && typeof el === 'object') {
-      // handle styles
-      let styles = parseStyles(el);
-      return `${acc} ${styles}>`;
-    } else {
-      // handle ???
-      return acc;
-    }
-  }, '');
-};
-
-const parseStyles = (stylesNode) => {
-  if (!stylesNode.hasOwnProperty('style')) {
-    throw new Error('Please pass a valid styles node.');
+const renderAtt = (att, value) => {
+  if (att === 'style') {
+    return renderStyles(value);
+  } else {
+    return `${att}="${value}"`;
   }
+};
 
-  const styles = Object.keys(stylesNode.style).map((key) => {
-    return `${key}: ${stylesNode.style[key]}`;
-  });
-  return `style="${styles.join('; ')}"`;
+const renderStyles = (stylesMap) => {
+  const stylesString = Object.keys(stylesMap).map((key) => {
+    return `${key}: ${stylesMap[key]}`;
+  }).join('; ');
+  return `style="${stylesString}"`;
 };
 
 const app = document.getElementById('app');
+console.log(render(nestedNode));
 console.log(render(rootNode));
-app.innerHTML = render(rootNode);
+app.innerHTML = render(nestedNode);
