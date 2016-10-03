dfa622ac2748700b92328ad409a3e6725606d70c
Sarah Ransohoff <sranso@gmail.com>
2016-10-03 12:17:17 -0400

    Halfway parse virtual node.

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
-const rootNode = document.createElement('div');
-rootNode.innerHTML = 'original DOM';
-rootNode.setAttribute('style', 'margin: 20px; font-size; 14px;');
+// patch
+// diff
+// create vd node itself
 
+// use https://github.com/paul-jean/dom-viz to see if correct
+// divs are changing
+
+// 1. describe virtual dom
+//    (as obj / list; start with list, expand as nec)
+// node : obj attributes : list html
+const rootNode = [
+  'div', {
+    'style': {
+      'textAlign': 'center',
+      'color': 'blue',
+      'margin': '20px'
+    }
+  },
+  ['hello world']
+];
+
+// 2. and take it to real dom (convert to html)
+const render = (node) => {
+  let html;
+  if (node.length !== 3) {
+    throw new Error('Please pass a valid node.');
+  }
+
+  return parseNode(node);
+};
+
+const parseNode = (node) => {
+  return node.reduce((acc, el, index) => {
+    if (typeof el === 'string') {
+      let styles = parseStyles(node[index + 1]);
+      return `${acc}<${el} ${styles}>`;
+    } else if (Array.isArray(el)) {
+      return acc;
+    } else {
+      return acc;
+    }
+  }, '');
+};
+
+const parseStyles = (stylesNode) => {
+  if (!stylesNode.hasOwnProperty('style')) {
+    throw new Error('Please pass a valid styles node.');
+  }
+
+  const styles = Object.keys(stylesNode.style).map((key) => {
+    return `${key}: ${stylesNode.style[key]}`;
+  });
+  return `style="${styles.join('; ')}"`;
+};
 
 const app = document.getElementById('app');
-app.innerHTML = '';
-app.appendChild(rootNode);
+app.innerHTML = render(rootNode);
