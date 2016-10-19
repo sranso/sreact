b14b93788c4723b53fdf1873e080dbc7351abaa1
Sarah Ransohoff <sranso@gmail.com>
2016-10-19 13:31:39 -0400

    WIP, use patch method.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ import render from './render';
 
 let count = 0;
 
-const rootTree = createNode('div', {}, [count]);
+let rootTree = createNode('div', {}, [count]);
 const domNode = document.getElementById('app');
 render(rootTree, domNode);
 
 setInterval(() => {
   count += 1;
   let newTree = createNode('div', {}, [count])
-  render(newTree, domNode);
+  let patches = diff(rootTree, newTree);
+  let result = patch(domNode, patches);
+  /*
+   * this works, but only because we're modifying the
+   * original DOM each time. we're not keeping track
+   * of the latest virtual DOM anymore.
+   * so, that's a TODO, hah.
+   */
+  // rootTree = result;
+  // render(newTree, domNode);
 }, 1000);
